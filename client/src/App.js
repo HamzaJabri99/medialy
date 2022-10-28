1a18efe0c44f90ef0b4c475ef9da2e905c8dab19
unknown <jabri.oldd@gmail.com>
2022-10-28 17:10:23 +0100

    setting-up app pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,10 @@
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Profile from "./pages/profile/Profile";
+import Register from "./pages/register/Register";
+
 function App() {
-  return <div className="App">Hôla</div>;
+  return <div className="App"></div>;
 }
 
 export default App;
