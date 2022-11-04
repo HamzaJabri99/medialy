1e2c930c3a08efbcd68b357999959fcb53d57d4d
unknown <jabri.oldd@gmail.com>
2022-11-04 17:02:33 +0100

    adding login functionality in backend

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,35 +1,53 @@
 import { db } from "../connect.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import cookie from "cookie-parser";
 export const register = (req, res) => {
-    //check if user exists
+  //check if user exists
   const q = "SELECT * FROM users WHERE username = ?";
   db.query(q, [req.body.username], (error, data) => {
     if (error) return res.status(500).json(error);
-    if(data.length) return res.status(409).json("user already exists!");
+    if (data.length) return res.status(409).json("user already exists!");
     //hash the password
-     const salt =bcrypt.genSaltSync(10);
-     const hashedPassword=bcrypt.hashSync(req.body.password,salt);
+    const salt = bcrypt.genSaltSync(10);
+    const hashedPassword = bcrypt.hashSync(req.body.password, salt);
     //create user
-     const q ="INSERT INTO users (`username`,`email`,`password`,`name`,`coverPicture`,`profilePicture`,`city`,`website`) VALUES (?)";
-     const values=[
-        req.body.username,
-        req.body.email,
-        hashedPassword,
-        req.body.name,
-        req.body.coverPicture,
-        req.body.profilePicture,
-        req.body.city,
-        req.body.website,
-     ];
-     db.query(q,[values],(error,data)=>{
-        if(error) return res.status(500).json("something went wrong");
-        return res.status(200).json("user has been registerd!");
-     })
+    const q =
+      "INSERT INTO users (`username`,`email`,`password`,`name`,`coverPicture`,`profilePicture`,`city`,`website`) VALUES (?)";
+    const values = [
+      req.body.username,
+      req.body.email,
+      hashedPassword,
+      req.body.name,
+      req.body.coverPicture,
+      req.body.profilePicture,
+      req.body.city,
+      req.body.website,
+    ];
+    db.query(q, [values], (error, data) => {
+      if (error) return res.status(500).json("something went wrong");
+      return res.status(200).json("user has been registerd!");
+    });
   });
-
-
 };
 export const login = (req, res) => {
+  //check user
+  const q = "SELECT * FROM users WHERE username = ?";
+  db.query(q, [req.body.username], (error, data) => {
+    if (error) return res.status(500).json(error);
+    if (data.length === 0) return res.status(404).json("no user was found");
 
+    if (!bcrypt.compareSync(req.body.password, data[0].password)) {
+      return res.status(400).json("username or password are incorrect");
+    }
+    const token = jwt.sign({ id: data[0].id }, "secretKey");
+    const { password, ...infos } = data[0];
+    return res.cookie("accessToken", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .json(infos);
+    
+  });
 };
 export const logout = (req, res) => {};
--- api/index.js
+++ api/index.js
@@ -1,4 +1,6 @@
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 const app = express();
 import authRouter from "./routes/auth.js";
 import userRouter from "./routes/users.js";
@@ -9,7 +11,8 @@ import likeRouter from "./routes/likes.js";
 //middlewares
 
 app.use(express.json());
-
+app.use(cors())
+app.use(cookieParser())
 
 
 //api endpoints
