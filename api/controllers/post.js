f8bb7decb040d2212181584571aabeb2dd9815cf
unknown <jabri.oldd@gmail.com>
2022-11-05 17:42:24 +0100

    adding addPost functionality in backend

--- api/controllers/post.js
+++ api/controllers/post.js
@@ -1,5 +1,6 @@
 import { db } from "../connect.js";
 import jwt from "jsonwebtoken";
+import moment from "moment";
 export const getPosts = (req, res) => {
   const token = req.cookies.accessToken;
   if (!token) return res.status(401).json("Not Logged in");
@@ -9,9 +10,29 @@ export const getPosts = (req, res) => {
     const q = `SELECT p.*, u.id AS userId, name, profilePicture  FROM posts AS p JOIN users AS u on (u.id = p.userId)
     LEFT JOIN fellowships AS f on (p.userId = f.followedUserId) WHERE f.followerUserId= ? OR p.userId= ?
     ORDER BY p.createdAt DESC`;
-    db.query(q, [user.id,user.id], (error, data) => {
+    db.query(q, [user.id, user.id], (error, data) => {
       if (error) return res.status(500).json(error);
       return res.status(200).json(data);
     });
+  });
+};
+
+export const addPost = (req, res) => {
+  const token = req.cookies.accessToken;
+  if (!token) return res.status(401).json("Not Logged in");
+  jwt.verify(token, "secretKey", (error, user) => {
+    if (error) return res.status(403).json("Token isn't valid");
+    const q = "INSERT INTO posts ('description','img','userId','createdAt') VALUES (?)";
+    const values = [
+      req.body.description,
+      req.body.img,
+      user.id,
+      new Date(),
+      moment(Date.now()).format("YYYY-MM-DD HH:MM:SS"),
+    ];
+    db.query(q,[values],(error,data)=>{
+      if (error) return res.status(500).json(error);
+      return res.status(200).json("Post has been published");
+    })
   });
 };
